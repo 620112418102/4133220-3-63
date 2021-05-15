ec355e85e0fb833539c8504b541e03f7cb548cf6
620112418102 <620112418102@bru.ac.th>
2021-05-15 16:30:59 +0700

    LAB9

--- LAB7/conBD.php
+++ LAB7/conBD.php
@@ -3,10 +3,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$host = "sql206.byethost4.com";
-$user = "b4_28370871";
-$pass = "Pna0960907988";
-$db= "b4_28370871_member";
+$host = "sql206.byethost9.com";
+$user = "b9_28370872";
+$pass = "@Anivat238841";
+$db= "b9_28370872_member";
 
 try{
     $conn = new mysqli($host,$user,$pass,$db);
